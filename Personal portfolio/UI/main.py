db15cd4431a073c9fbfea8833ce039487dd0178a
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-04-01 13:41:16 -0600

    eeee

---
+++ Personal portfolio/UI/main.py
@@ -0,0 +1,12 @@
+#Tate K. Morgan's personal collection of fun projects
+
+
+
+
+
+
+
+
+def ui():
+    print("Welcome to Tate K. Morgan's personal portfolio of fun projects \nWe have many things for you to try \nPlease press the corresponding number")
+    print("1. ___ \n2. ___ \n3. ___ \n4. ___ \n5. ___ \n6. ___ \n7. ___ \n8. ___ \n9. ___ \n10. ___")
